f51d2b9b59544441b0c1c14eb70227f866f80505
Anjan Roy <hello@itzmeanjan.in>
2025-07-14 11:37:44 +0400

    Update how file, filled with random data is created for testing decds CLI
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test_decds_on_linux.sh
+++ test_decds_on_linux.sh
@@ -1,45 +1,45 @@
 #!/usr/bin/bash
 
 # Generate random 256MB data blob
-head -c 256M /dev/urandom > random.data
+dd if=/dev/urandom of=random.data bs=1M count=256
 
 # Break blob into chunksets and verify each chunk's validity
 cargo run --profile optimized -- break -b random.data -o broken
 cargo run --profile optimized -- verify broken
 
 # Repair chunksets and check SHA256 digest of original data blob and repaired data blob
-cargo run --profile optimized -- repair -c broken -o repaired-with-16
-echo "$(sha256sum random.data | awk '{print $1}') repaired-with-16/repaired.data" | sha256sum --check
+cargo run --profile optimized -- repair -c broken -o repairing-with-16
+echo "$(sha256sum random.data | awk '{print $1}') repairing-with-16/repaired.data" | sha256sum --check
 
 # Mutate a single byte of a proof-carrying chunk belonging to chunkset-15
 # Repairing with 15 valid chunks for chunkset-15 - must work!
 dd if=/dev/urandom of=broken/chunkset.15/share00.data bs=1 seek=1 count=1 conv=notrunc
-cargo run --profile optimized -- repair -c broken -o repaired-with-15
-echo "$(sha256sum random.data | awk '{print $1}') repaired-with-15/repaired.data" | sha256sum --check
+cargo run --profile optimized -- repair -c broken -o repairing-with-15
+echo "$(sha256sum random.data | awk '{print $1}') repairing-with-15/repaired.data" | sha256sum --check
 
 # Mutate a single byte of a proof-carrying chunk belonging to chunkset-15
 # Repairing with 14 valid chunks for chunkset-15 - must work!
 dd if=/dev/urandom of=broken/chunkset.15/share02.data bs=1 seek=11 count=1 conv=notrunc
-cargo run --profile optimized -- repair -c broken -o repaired-with-14
-echo "$(sha256sum random.data | awk '{print $1}') repaired-with-14/repaired.data" | sha256sum --check
+cargo run --profile optimized -- repair -c broken -o repairing-with-14
+echo "$(sha256sum random.data | awk '{print $1}') repairing-with-14/repaired.data" | sha256sum --check
 
 # Mutate a single byte of a proof-carrying chunk belonging to chunkset-15
 # Repairing with 13 valid chunks for chunkset-15 - must work!
 dd if=/dev/urandom of=broken/chunkset.15/share04.data bs=1 seek=111 count=1 conv=notrunc
-cargo run --profile optimized -- repair -c broken -o repaired-with-13
-echo "$(sha256sum random.data | awk '{print $1}') repaired-with-13/repaired.data" | sha256sum --check
+cargo run --profile optimized -- repair -c broken -o repairing-with-13
+echo "$(sha256sum random.data | awk '{print $1}') repairing-with-13/repaired.data" | sha256sum --check
 
 # Mutate a single byte of a proof-carrying chunk belonging to chunkset-15
 # Repairing with 12 valid chunks for chunkset-15 - must work!
 dd if=/dev/urandom of=broken/chunkset.15/share15.data bs=1 seek=1 count=1 conv=notrunc
-cargo run --profile optimized -- repair -c broken -o repaired-with-12
-echo "$(sha256sum random.data | awk '{print $1}') repaired-with-12/repaired.data" | sha256sum --check
+cargo run --profile optimized -- repair -c broken -o repairing-with-12
+echo "$(sha256sum random.data | awk '{print $1}') repairing-with-12/repaired.data" | sha256sum --check
 
 # Mutate a single byte of a proof-carrying chunk belonging to chunkset-15
 # Repairing with 11 valid chunks for chunkset-15 - must work!
 dd if=/dev/urandom of=broken/chunkset.15/share12.data bs=1 seek=100 count=1 conv=notrunc
-cargo run --profile optimized -- repair -c broken -o repaired-with-11
-echo "$(sha256sum random.data | awk '{print $1}') repaired-with-11/repaired.data" | sha256sum --check
+cargo run --profile optimized -- repair -c broken -o repairing-with-11
+echo "$(sha256sum random.data | awk '{print $1}') repairing-with-11/repaired.data" | sha256sum --check
 
 # Note:
 # It should ideally be possible to recover a chunkset with 10 valid chunks.
@@ -55,7 +55,7 @@ dd if=/dev/urandom of=broken/chunkset.15/share09.data bs=1 seek=781 count=1 conv
 dd if=/dev/urandom of=broken/chunkset.15/share07.data bs=1 seek=223 count=1 conv=notrunc
 
 # Now trying to repair, with 9 valid chunks for chunkset-15, it should fail with return code 1, as chunkset-15 can't be recovered.
-cargo run --profile optimized -- repair -c broken -o repaired-with-9 | tee console.out; test ${PIPESTATUS[0]} -eq 1
+cargo run --profile optimized -- repair -c broken -o repairing-with-9 | tee console.out; test ${PIPESTATUS[0]} -eq 1
 
 # Clean up
-rm -rf random.data broken repaired* console.out
+rm -rf random.data broken repairing-with* console.out
